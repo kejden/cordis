e90fa43c3815b42dc42bef18f6d3160536802ff0
Gabriel Charkiewicz <gabryel.2002@gmail.com>
2024-11-16 12:49:22 +0100

    Project backbone

---
+++ cordis-backend/src/main/java/io/ndk/cordis_backend/CordisBackendApplication.java
@@ -0,0 +1,13 @@
+package io.ndk.cordis_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CordisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CordisBackendApplication.class, args);
+	}
+
+}
---
+++ cordis-backend/src/test/java/io/ndk/cordis_backend/CordisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ndk.cordis_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CordisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
